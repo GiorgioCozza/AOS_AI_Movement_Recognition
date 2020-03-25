f1769e4b3cc8067cc465fe1c1667e3aaf8504275
Giorgio Cozza <10649461@polimi.it>
2020-03-25 10:30:14 +0100

    Title: Project Report Desc: Detailed report of the design and implementation process and related results.

---
+++ reports/proj_doc/code/config.py
@@ -0,0 +1,46 @@
+
+import os
+
+# CLASS LABELS
+LABELS = ["running",
+          "walking",
+          "jumping",
+          "standing",
+          "sitting",
+          "supine",
+          "lying_on_side"]
+
+num_classes = len(LABELS)
+# HW INFO
+SENSORS = ["ACC_LSM6DSL_DS", "GYR_LSM6DSL_DS", "ACC_LSM303AGR_DS", "MAG_LSM303AGR_DS"]
+AXIS = ["x", "y", "z"]
+
+
+# DATASET AND SETTINGS INFO
+workspace = os.getcwd()
+datasets_dir = os.path.join(workspace, "../datasets/")
+session_dir = os.path.join(datasets_dir, "activity_files")
+run_ds_path = os.path.join(datasets_dir, "running.txt")
+jump_ds_path = os.path.join(datasets_dir, "jumping.txt")
+stand_ds_path = os.path.join(datasets_dir, "standing.txt")
+walk_ds_path = os.path.join(datasets_dir, "walking.txt")
+sit_ds_path =os.path.join( datasets_dir, "sitting.txt")
+sup_ds_path = os.path.join(datasets_dir, "supine.txt")
+los_ds_path = os.path.join(datasets_dir, "lying_on_side.txt")
+
+plot_dir = "../plots"
+log_dir = "../log/"
+curlog_dir = ""
+plot_dir = "../plots"
+mod_dir = "../saved_models"
+csv_dir = "../test_csv"
+best_mod_dir = "../best_models"
+
+cmod = {'CNN': True, 'RNN': False}
+
+# DATA PROCESSING INFO
+WINDOW_SAMPLES = 30
+SENS_VALUES = 12
+ds_model = {"ACC_LSM6DSL_DS": {}, "GYR_LSM6DSL_DS": {}, "ACC_LSM303AGR_DS": {}, "MAG_LSM303AGR_DS": {}}
+
+
